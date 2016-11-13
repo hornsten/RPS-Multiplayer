e26b54d27a6f58d06a52c5e6c2cd55ac39247ddf
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-13 12:17:02 -0600

    Players can be pushed to Firebase, but not nested, and need to track number of players in the database

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,32 +8,63 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
+var database = firebase.database();
+var username = "";
+var dateAdded = "";
+var numPlayers = 0;
+var players;
+var wins = 0;
+var losses = 0;
 
 //Sign-in and Authorization
 
-firebase.auth().signInAnonymously().catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
+$('#start').on('click', function() {
+
+    if (numPlayers > 1) {
+
+        alert('Too many players.  Try again later');
+    } else {
+        username = $('#username').val().trim();
+
+        $('#username').val("");
+        numPlayers++;
+        console.log(numPlayers);
+        database.ref().push({
+            username: username,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP,
+            wins: wins,
+            losses: losses
+
+        });
+
+    };
+    return false;
 });
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        // User is signed in.
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        // ...
-    } else {
-        // User is signed out.
-        // ...
-    }
-    // ...
-});
+// firebase.auth().signInAnonymously().catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+// });
+
+// firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+//         // User is signed in.
+//         var isAnonymous = user.isAnonymous;
+//         var uid = user.uid;
+//         // ...
+//     } else {
+//         // User is signed out.
+//         // ...
+//     }
+//     // ...
+// });
+
+
 
 // Buttons are functioning-----------------------------------
-$('#start').on('click', holla);
+
 $('#chat').on('click', holla);
 
 function holla() {
@@ -45,5 +76,5 @@ function holla() {
 $('#player-1').html('<h2>Player 1</h2>');
 $('#game-results').html('<h2>Results</h2>');
 $('#player-2').html('<h2>Player 2</h2>');
-$('#chat-log').html('<p>braaaaa u got served</p>');
+$('#chat-log').append('<p>braaaaa u got served</p>');
 //-----------------------------------------------------------
