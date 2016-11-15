5852bf8909fe54be451efb622e00504290d650b7
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-15 07:05:07 -0600

    A second player can join the game

--- assets/javascript/logic2.js
+++ assets/javascript/logic2.js
@@ -9,51 +9,122 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database().ref();
-
 var player1 = "";
+var player2 = "";
+var wins2 = 0;
+var losses2 = 0;
 var wins1 = 0;
 var losses1 = 0;
-var player1db = database.child('players').child('1');
+var gameStarted = false;
+var gameClosed = false;
+
+
+$('#join-game').hide();
+$('#joiner').hide();
+
 $('#start').on('click', function() {
 
+    firebase.auth().signInAnonymously();
+
+    var uid = firebase.auth().currentUser.uid;
 
     player1 = $('#username').val().trim();
 
-    player1db.set({
+    database.child('players').child('1').set({
 
         player1: player1,
         wins1: wins1,
-        losses1: losses1
+        losses1: losses1,
+        uid: uid,
+        gameStarted: true
     });
 
 
     $('#start').hide();
     $('#username').hide();
 
-
 });
 
 //Firebase watcher + initial loader HINT: .on("value")
-player1db.on("value", function(snapshot) {
+database.child('players').child('1').on("value", function(snapshot) {
 
     //Log everything that's coming out of snapshot
-    // console.log(snapshot.val());
     console.log(snapshot.val());
-    // console.log(snapshot.val().player2);
-    // console.log(snapshot.val().results);
-    // console.log(snapshot.val().chat);
 
     // Change the HTML to reflect
     $('#player-1').html('<h2>' + snapshot.val().player1 + '</h2>')
-        .append('<h4>Wins: ' + snapshot.val().wins1 + ' </h4>')
-        .append('<h4>Losses:' + snapshot.val().losses1 + ' </h4>');
-    $("#player-2").html(snapshot.val().player2);
-    $("#game-results").html(snapshot.val().results);
-    $("#chat-log").html(snapshot.val().chat);
+        .append('<h4>Wins: ' + snapshot.val().wins1 + '</h4>')
+        .append('<h4>Losses: ' + snapshot.val().losses1 + '</h4>');
+
+    $('#join-game').show();
+    $('#joiner').show();
 
     // Handle the errors
 }, function(errorObject) {
 
     console.log("Errors handled: " + errorObject.code);
+
+});
+
+
+//Auth Listener
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+
+
+    database.child('players').child('1').remove();
+    numPlayers = 0;
+
+});
+
+$('#join-game').on('click', function() {
+
+    firebase.auth().signInAnonymously();
+
+    var uid2 = firebase.auth().currentUser.uid;
+
+    player2 = $('#joiner').val().trim();
+
+    database.child('players').child('2').set({
+
+        player2: player2,
+        wins2: wins2,
+        losses2: losses2,
+        uid2: uid2,
+        gameStarted: true
+    });
+
+
+    $('#join-game').hide();
+    $('#joiner').hide();
+
+});
+
+//Firebase watcher + initial loader HINT: .on("value")
+database.child('players').child('2').on("value", function(snapshot) {
+
+    //Log everything that's coming out of snapshot
+    console.log(snapshot.val());
+
+    // Change the HTML to reflect
+    $('#player-2').html('<h2>' + snapshot.val().player2 + '</h2>')
+        .append('<h4>Wins: ' + snapshot.val().wins2 + '</h4>')
+        .append('<h4>Losses: ' + snapshot.val().losses2 + '</h4>');
+
+    // Handle the errors
+}, function(errorObject) {
+
+    console.log("Errors handled: " + errorObject.code);
+
+});
+
+
+//Auth Listener
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+
+
+    database.child('players').child('2').remove();
+    numPlayers = 0;
 
 });
